d8e04b39451d64c14f75c0e9132837166cc94ce4
EuropeanPepe <46200173+encomjp@users.noreply.github.com>
2024-12-13 10:05:14 +0100

    Add files via upload

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -933,7 +933,7 @@ const Chat = () => {
               </Stack>
               <QuestionInput
                 clearOnSend
-                placeholder="Type a new question..."
+                placeholder="Neue Frage stellen..."
                 disabled={isLoading}
                 onSend={(question, id) => {
                   appStateContext?.state.isCosmosDBAvailable?.cosmosDB
